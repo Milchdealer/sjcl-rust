473a2e921ba8d54e21cfd03bbf8da4089ecaa737
Teraku <teraku@teraku.de>
2021-03-28 17:05:56 +0200

    First fully functional release
    Now `AES-CCM` is supported.
    You can use this [demo tool](https://bitwiseshiftleft.github.io/sjcl/demo/) to
    verify.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sjcl-rust
 Library which supports decrypting data that was encrypted using SJCL.
+
+## Features
+- [x] `AES-CCM`
+- [ ] `AES-OCB2`
+
+## Usage
+`Cargo.toml`:
+```toml
+sjcl = "0.0.1"
+```
+
+```rust
+use sjcl::decrypt_raw;
+
+let data = "{\"iv\":\"nJu7KZF2eEqMv403U2oc3w==\", \"v\":1, \"iter\":10000, \"ks\":256, \"ts\":64, \"mode\":\"ccm\", \"adata\":\"\", \"cipher\":\"aes\", \"salt\":\"mMmxX6SipEM=\", \"ct\":\"VwnKwpW1ah5HmdvwuFBthx0=\"}".to_string();
+let password_phrase = "abcdefghi".to_string();
+
+let plaintext = decrypt_raw(data, password_phrase).?;
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
+//! # sjcl
+//! Simple decrypt-only SJCL library.
+//!
+//! Only supports AES-CCM so far, but OCB2 is deprecated AFAIK.
+//! To use you only need the result of a SJCL encrypted secret and the
+//! passphrase:
+//!
+//! ## Usage
+//! Packagename for your `Cargo.toml`:
+//! ```toml
+//! sjcl = "0.0.1"
+//! ```
+//!
+//! Decrypt a file loaded into a string:
+//! ```rust
+//! use sjcl::decrypt_raw;
+//! 
+//! let data = "{\"iv\":\"nJu7KZF2eEqMv403U2oc3w==\", \"v\":1, \"iter\":10000, \"ks\":256, \"ts\":64, \"mode\":\"ccm\", \"adata\":\"\", \"cipher\":\"aes\", \"salt\":\"mMmxX6SipEM=\", \"ct\":\"VwnKwpW1ah5HmdvwuFBthx0=\"}".to_string();
+//! let password_phrase = "abcdefghi".to_string();
+//! let plaintext = decrypt_raw(data, password_phrase)?;
+//! ```
+//!
+//! This will give you the plaintext `test\ntest`.
 extern crate base64;
 
-use ccm::{Ccm, consts::{U8}};
+use ccm::{Ccm, consts::{U8, U13}};
 use ccm::aead::{Aead, NewAead, generic_array::GenericArray};
-use aes::{Aes128, Aes256};
+use aes::Aes256;
+use password_hash::{SaltString, PasswordHasher};
+use pbkdf2::{Pbkdf2, Params};
 use serde::Deserialize;
+use serde_json;
 
 use snafu::Snafu;
 #[derive(Debug, Snafu)]
@@ -16,11 +42,11 @@ pub enum SjclError {
 
 #[derive(Debug, Deserialize)]
 pub struct SjclBlockJson {
-    iv: String,  // Nonce
-    v: i32,
+    iv: String,
+    v: u32,
     iter: u32,
-    ks: u32,
-    ts: u32,
+    ks: usize,
+    ts: usize,
     mode: String,
     adata: String,
     cipher: String,
@@ -28,42 +54,110 @@ pub struct SjclBlockJson {
     ct: String,
 }
 
-type AesCcm128 = Ccm<Aes128, U8, U8>;
-type AesCcm256 = Ccm<Aes256, U8, U8>;
+type AesCcm = Ccm<Aes256, U8, U13>;
 
-pub fn decrypt(chunk: SjclBlockJson, key: String) -> Result<String, SjclError> {
+/// Decrypts a chunk of SJCL encrypted JSON with a given passphrase.
+pub fn decrypt_raw(chunk: String, key: String) -> Result<String, SjclError> {
+    match serde_json::from_str(&chunk) {
+        Ok(chunk) => decrypt(chunk, key),
+        Err(_) => return Err(SjclError::DecryptionError {
+            message: "Failed to parse JSON".to_string(),
+        }),
+    }
+}
+
+/// Utility function to trim the initialization vector to the proper size of
+/// the nonce.
+/// (See: [SJCL/core.ccm.js](https://github.com/bitwiseshiftleft/sjcl/blob/master/core/ccm.js#L61))
+fn truncate_iv(mut iv: Vec<u8>, output_size: usize, tag_size: usize) -> Vec<u8> {
+    let iv_size = iv.len();
+    let output_size = (output_size - tag_size) / 8;
+
+    let mut l = 2;
+    while l < 4 && ((output_size >> (8 * l)) > 0) {
+        l += 1
+    }
+    if iv_size <= 15 && l < 15 - iv_size {
+        l = 15 - iv_size
+    }
+
+    let _ = iv.split_off(15 - l);
+    iv
+}
+
+/// Decrypts a chunk of SJCL encrypted JSON with a given passphrase.
+pub fn decrypt(mut chunk: SjclBlockJson, key: String) -> Result<String, SjclError> {
     match chunk.cipher.as_str() {
         "aes" => {
             match chunk.mode.as_str() {
                 "ccm" => {
-                    let nonce = match base64::decode(chunk.iv) {
-                        Ok(v) => v,
+                    if chunk.v != 1 {
+                        return Err(SjclError::DecryptionError {
+                            message: "Only version 1 is currently supported".to_string(),
+                        });
+                    }
+                    if chunk.adata.len() > 0 {
+                        return Err(SjclError::DecryptionError {
+                            message: "Expected empty additional data".to_string(),
+                        });
+                    }
+
+                    let salt_str = match base64::decode(chunk.salt) {
+                        Ok(v) => SaltString::b64_encode(&v),
                         Err(_) => return Err(SjclError::DecryptionError {
-                            message: "Failed to read IV".to_string(),
+                            message: "Failed to base64 decode salt".to_string(),
                         }),
                     };
-                    let nonce = GenericArray::from_slice(&nonce);
-                    let key = GenericArray::from_slice(key.as_bytes());
-                    if chunk.ks == 256 {
-                        let cipher = AesCcm256::new(key);
+                    let salt = salt_str.unwrap();
+                    let password_hash = Pbkdf2.hash_password(
+                        key.as_bytes(),
+                        None, 
+                        None,
+                        Params{rounds: chunk.iter, output_length: chunk.ks / 8},
+                        salt.as_salt(),
+                    );
+                    let password_hash = match password_hash {
+                        Ok(pwh) => pwh,
+                        Err(_) => return Err(SjclError::DecryptionError {
+                            message: "Failed to generate password hash".to_string(),
+                        }),
+                    };
+                    let password_hash = password_hash.hash.unwrap();
 
-                        let plaintext = cipher.decrypt(nonce, chunk.ct.as_ref());
-                    } else if chunk.ks == 128 {
-                        let cipher = AesCcm256::new(key);
-
-                        let plaintext = cipher.decrypt(nonce, chunk.ct.as_ref());
+                    // Fix missing padding
+                    for _ in 0..(chunk.iv.len() % 4) {
+                        chunk.iv.push('=');
                     }
-
-                    Err(SjclError::NotImplementedError)
+                    for _ in 0..(chunk.ct.len() % 4) {
+                        chunk.ct.push('=');
+                    }
+                    let iv = match base64::decode(chunk.iv) {
+                        Ok(v) => v,
+                        Err(_) => return Err(SjclError::DecryptionError {
+                            message: "Failed to decode IV".to_string(),
+                        }),
+                    };
+                    let ct = match base64::decode(chunk.ct) {
+                        Ok(v) => v,
+                        Err(_) => return Err(SjclError::DecryptionError {
+                            message: "Failed to decode ct".to_string(),
+                        }),
+                    };
+                    let iv = truncate_iv(iv, ct.len() * 8, chunk.ts);
+                    let nonce  = GenericArray::from_slice(iv.as_slice());
+                    let key = GenericArray::from_slice(password_hash.as_bytes());
+                    let cipher = AesCcm::new(key);
+                    let plaintext = match cipher.decrypt(nonce, ct.as_ref()) {
+                        Ok(pt) => pt,
+                        Err(_) => {
+                            return Err(SjclError::DecryptionError {
+                                message: "Failed to decrypt ciphertext".to_string(),
+                            });
+                        },
+                    };
+                    Ok(String::from_utf8(plaintext).unwrap())
                  },
                 "ocb2" => {
-                    if chunk.ks == 128 && chunk.iter == 1000 && chunk.ts == 64 {
-                        
-                    } else if chunk.ks == 256 && chunk.iter == 10000 && chunk.ts == 64 {
-                        
-                    } else {
-                        
-                    }
                     Err(SjclError::NotImplementedError)
                 },
                 _ => Err(SjclError::NotImplementedError)
@@ -73,10 +167,39 @@ pub fn decrypt(chunk: SjclBlockJson, key: String) -> Result<String, SjclError> {
     }
 }
 
+/// https://bitwiseshiftleft.github.io/sjcl/demo/
 #[cfg(test)]
 mod tests {
+    use crate::{decrypt, decrypt_raw, SjclBlockJson};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_end_to_end() {
+        let data = "{\"iv\":\"nJu7KZF2eEqMv403U2oc3w==\", \"v\":1, \"iter\":10000, \"ks\":256, \"ts\":64, \"mode\":\"ccm\", \"adata\":\"\", \"cipher\":\"aes\", \"salt\":\"mMmxX6SipEM=\", \"ct\":\"VwnKwpW1ah5HmdvwuFBthx0=\"}".to_string();
+        let password_phrase = "abcdefghi".to_string();
+
+        let plaintext = "test\ntest".to_string();
+
+        assert_eq!(decrypt_raw(data, password_phrase).unwrap(), plaintext);
+    }
+
+    #[test]
+    fn test_with_struct() {
+        let data = SjclBlockJson {
+            iv: "nJu7KZF2eEqMv403U2oc3w".to_string(),
+            v: 1,
+            iter: 10000,
+            ks: 256,
+            ts: 64,
+            mode: "ccm".to_string(),
+            adata: "".to_string(),
+            cipher: "aes".to_string(),
+            salt: "mMmxX6SipEM".to_string(),
+            ct: "VwnKwpW1ah5HmdvwuFBthx0=".to_string(),
+        };
+        let password_phrase = "abcdefghi".to_string();
+
+        let plaintext = "test\ntest".to_string();
+
+        assert_eq!(decrypt(data, password_phrase).unwrap(), plaintext);
     }
 }
